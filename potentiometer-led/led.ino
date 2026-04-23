99c212143f63e6c908cf5c30cd70707eeb2a1a7d
Armaan Watts <armaan.watts2007@gmail.com>
2026-04-23 14:21:26 +0530

    Add LED brightness control using potentiometer

---
+++ potentiometer-led/led.ino
@@ -0,0 +1,14 @@
+int potPin = A0;
+int ledPin = 9;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);   // 0 to 1023
+
+  int brightness = map(potValue, 0, 1023, 0, 255);
+
+  analogWrite(ledPin, brightness);     // control LED brightness
+}
